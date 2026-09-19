ceaddfcd016f015aaf8d4354e20d6d18c7441f76
Yeabsera Fikadu <carlosqebero20@gmail.com>
2026-09-19 20:35:04 +0300

    Update nodemailer transport to port 587

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -12,7 +12,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
